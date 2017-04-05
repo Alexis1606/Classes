64bc3f29572d43bde1719d99c4d88f14a909eabb
Alexis <Alexis@DESKTOP-QHD09S2>
2017-04-05 17:09:46 -0500

    Agregado query para base de datos

---
+++ Classes/Classes/MSSql.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class MSSql
+    {
+        public static string FirstDataFromTable(DataTable dt)
+        {
+            string res;
+            DataRow dr = dt.Rows[0];
+            res = dr[0].ToString();
+            return res;
+        }
+        public static DataTable ExecuteStoredProcedure(string ConnectionString, string stored, Parameter[] parameters)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand com = new SqlCommand(stored);
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                string name = parameters[i].name;
+                object value = parameters[i].value;
+                com.Parameters.Add(new SqlParameter(name, value));
+            }
+            com.CommandType = CommandType.StoredProcedure;
+            com.Connection = con;
+            DataTable dt = new DataTable();
+            con.Open();
+            dt.Load(com.ExecuteReader());
+            con.Close();
+            return dt;
+        }
+        public static string FirstDataFromTable(string ConnectionString, string stored, Parameter[] parameters)
+        {
+            DataTable dt = ExecuteStoredProcedure(ConnectionString, stored, parameters);
+            return FirstDataFromTable(dt);
+        }
+    }
+}
